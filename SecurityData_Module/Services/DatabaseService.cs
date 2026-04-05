09e89f68be4910d1e2db6408920fb79335b7ef92
ssaw2705 <24521263@gm.uit.edu.vn>
2026-04-05 20:19:03 +0700

    Thêm logic Mã hóa AES và SQLite

---
+++ SecurityData_Module/Services/DatabaseService.cs
@@ -0,0 +1,66 @@
+﻿using SecurityData.Models;
+using SecurityData.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SecurityData.Services
+{
+    public class DatabaseService
+    {
+        private string dbPath = "LocalChat.db";
+
+        public DatabaseService()
+        {
+            if (!File.Exists(dbPath)) SQLiteConnection.CreateFile(dbPath);
+            ExecuteNonQuery("CREATE TABLE IF NOT EXISTS Messages (Id INTEGER PRIMARY KEY AUTOINCREMENT, Sender TEXT, Content TEXT, IsFile INTEGER, Time DATETIME)");
+        }
+
+        private void ExecuteNonQuery(string sql)
+        {
+            using (var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+            {
+                conn.Open();
+                new SQLiteCommand(sql, conn).ExecuteNonQuery();
+            }
+        }
+
+        public void SaveMessage(string sender, string encryptedContent, bool isFile)
+        {
+            using (var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Messages (Sender, Content, IsFile, Time) VALUES (@s, @c, @f, datetime('now'))";
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@s", sender);
+                cmd.Parameters.AddWithValue("@c", encryptedContent);
+                cmd.Parameters.AddWithValue("@f", isFile ? 1 : 0);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<ChatMessage> GetHistory()
+        {
+            var list = new List<ChatMessage>();
+            using (var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+            {
+                conn.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Messages ORDER BY Time ASC", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new ChatMessage
+                        {
+                            Sender = reader["Sender"].ToString(),
+                            Content = reader["Content"].ToString(),
+                            IsFile = Convert.ToInt32(reader["IsFile"]) == 1
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ SecurityData_Module/Services/FileTransferService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace SecurityData.Services
+{
+    public class FileTransferService
+    {
+        // Hàm chặt file ra thành mảng byte để gửi đi
+        public static byte[] FileToBytes(string filePath)
+        {
+            return File.ReadAllBytes(filePath);
+        }
+
+        // Hàm nhận mảng byte và lưu lại thành file
+        public static void BytesToFile(string fileName, byte[] data)
+        {
+            string savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ChatApp_Files");
+            if (!Directory.Exists(savePath)) Directory.CreateDirectory(savePath);
+
+            File.WriteAllBytes(Path.Combine(savePath, fileName), data);
+        }
+    }
+}
